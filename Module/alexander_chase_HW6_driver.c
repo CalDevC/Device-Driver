250a521495c40826d3543cc32f377bec3000bacc
Chase Alexander <mr.alexander5132@gmail.com>
2022-05-07 09:38:56 -0700

    Completed init, open, close, and started app

--- Module/alexander_chase_HW6_driver.c
+++ Module/alexander_chase_HW6_driver.c
@@ -8,23 +8,55 @@
 * File: alexander_chase_HW6_driver.c
 *
 * Description:
+*
 **************************************************************/
-#include <linux/init.h>
+
 #include <linux/module.h>
 #include <linux/cdev.h>
-#include <linux/uaccess.h>
 #include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include <vmalloc.h>
 
 #define MY_MAJOR 415
 #define MY_MINOR 0
+#define DEVICE_NAME "TextModder"
+
+int major, minor;
+char* kernel_buffer;
+
+struct cdev my_cdev;
+int actual_rx_size = 0;
+
+MODULE_AUTHOR("Chase Alexander");
+MODULE_DESCRIPTION(
+  "A simple program to modify your text to either all upper case, all lower case, or reversed"
+);
+MODULE_LICENCE("GPL");
+
+//Data structure to keep track of how many times data is written
+struct driverDS {
+  int bufferPos;  //Buffer position
+  char buffer[200];
+} driverDS;
 
 static int device_open(struct inode* inode, struct file* fs) {
+  //Allocate memory for driver data and save it to private_data
+  struct driverDS* ds = vmalloc(sizeof(struct driverDS));
+
+  if (ds == 0) {
+    printk(KERN_ERR "vmalloc could not be completed: file not opened\n");
+  }
+
+  ds->bufferPos = 0;
+  fs->private_data = ds;
   return 0;
 }
 
 static int device_release(struct inode* inode, struct file* fs) {
+  //Release allocated memory for driver data
+  struct driverDS* ds = (struct driverDS*)fs->private_data;
+  vfree(ds);
   return 0;
 }
 
@@ -44,15 +76,36 @@ static long device_ioctl(struct file* fs, unsigned int command,
 }
 
 struct file_operations fops = {
-    .open = device_open,
-    .release = device_release,
-    .read = device_read,
-    .write = device_write,
-    .unlocked_ioctl = device_ioctl
+  .owner = THIS_MODULE,
+  .open = device_open,
+  .release = device_release,
+  .read = device_read,
+  .write = device_write,
+  .unlocked_ioctl = device_ioctl
 };
 
 int init_module(void) {
-  return 0;
+  int result, registers;
+  dev_t devno;
+
+  devno = MKDEV(MY_MAJOR, MY_MINOR);
+
+  //register device driver
+  registers = register_chrdev_region(devno, 1, DEVICE_NAME);
+  printk(KERN_INFO "Register chrdev succeeded 1: %d\n", registers);
+  cdev_init(&my_cdev, &fops);
+  my_cdev.owner = THIS_MODULE;
+
+  result = cdev_add(&my_cdev, devno, 1);
+
+  printk(KERN_INFO "Dev Added chrdev succeeded 2: %d\n", result);
+  printk(KERN_INFO "Device driver loaded\n");
+
+  if (result < 0) {
+    printk(KERN_ERR "Register chrdev failed: %d\n", result);
+  }
+
+  return result;
 }
 
 void cleanup_module(void) {
--- Test/alexander_chase_HW6_main.c
+++ Test/alexander_chase_HW6_main.c
@@ -7,12 +7,35 @@
 *
 * File: alexander_chase_HW6_main.c
 *
-* Description:
+* Description: A simple sample application to test my device driver
+*
 **************************************************************/
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char* argv[]) {
+  int fd, info;
+  long n1, n2, n3;
+  char message[] = { "Sample Message" };
+  char read_buf[512];
 
-  return 0;
+  fd = open("/dev/TextModder", O_RDWR);
+  printf("returned from open file, %d\n", fd);
+  if (fd < 0) {
+    printf("Device Open Error\n");
+    perror("Device File Open Error");
+    return -1;
+  } else {
+    printf("Device open success.\n");
+  }
+
+  //Do Work
+
+  close(fd);
 }
